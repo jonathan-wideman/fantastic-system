bb3167900c192ce4ccfea0b6de137996dccebab4
jonathan-wideman <jonathan.m.wideman@gmail.com>
2016-10-19 17:23:45 -0400

    Made followers seek the nearest position in a formation

--- Assets/_Scripts/Follower.cs
+++ Assets/_Scripts/Follower.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Follower : MonoBehaviour {
 
-	public Transform target;
+	public Formation formation;
+	//public Transform target;
+	public Vector3 targetPos;
+	public bool hasTarget;
 	public Vector3 offset;
 	public float repathDelay = 0.5f;
+	public bool assignedFormation = false;
 
 	NavMeshAgent navAgent;
 	NavMeshHit destination;
@@ -23,12 +27,29 @@ public class Follower : MonoBehaviour {
 
 	IEnumerator Repath () {
 		while (true) {
-			if (target != null) {
-				if (NavMesh.SamplePosition (target.position + target.rotation * offset, out destination, 10f, NavMesh.AllAreas)) {
+			/*
+			if (formation != null) {
+				//if (NavMesh.SamplePosition (target.position + target.rotation * offset, out destination, 10f, NavMesh.AllAreas)) {
+				if (NavMesh.SamplePosition (formation.GetClosestOpenPosition (transform.position), out destination, 10f, NavMesh.AllAreas)) {
+					navAgent.SetDestination (destination.position);
+				}
+			}
+			*/
+			if (hasTarget) {
+				if (NavMesh.SamplePosition (targetPos, out destination, 10f, NavMesh.AllAreas)) {
 					navAgent.SetDestination (destination.position);
 				}
 			}
 			yield return new WaitForSeconds (repathDelay);
 		}
+	}
+
+	public void SetTarget (Vector3 pos) {
+		hasTarget = true;
+		targetPos = pos;
+	}
+
+	public void ReleaseTarget () {
+		hasTarget = false;
 	}
 }
