5dc986bf88235a46a21dfdaddc23f2b3ac2ffc25
jonathan-wideman <jonathan.m.wideman@gmail.com>
2016-10-19 23:45:57 -0400

    Added a simple random turn order and attacks during battle
    - Battlers can now take and deal damage, and become defeated at 0 health
    - BattleController now adds all battlers to a random turn order when
      battle begins, and cycles turns until battle is complete
    - Battlers automatically attack on their turn, dealing a set damage amount
      to a random enemy target

--- Assets/_Scripts/BattleController.cs
+++ Assets/_Scripts/BattleController.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BattleController : MonoBehaviour {
 
 	GameController game;
 
 	public bool complete;
+	public int roundCount;
 	public int turnCount;
 
 	public Team[] teams;
 
 	Team winner;
+
+	List<Battler> turnOrder;
 
 	void Start () {
 		game = GameController.Instance ();
@@ -20,21 +24,46 @@ public class BattleController : MonoBehaviour {
 		if (complete == false) {
 			game.state = GameController.GameState.Battle;
 			turnCount = 0;
+			roundCount = 0;
 			winner = null;
-			StartCoroutine (TakeTurns ());
+			CreateTurnOrder ();
+
+			StartCoroutine (ProcessBattle ());
 		} else {
 			Debug.LogWarning ("Tried to begin a completed battle: " + this);
 		}
 	}
 
-	IEnumerator TakeTurns () {
+	IEnumerator ProcessBattle () {
+		Debug.Log ("======== BATTLE BEGINS ========");
+
+		Battler currentBattler;
 		while (complete == false) {
-			Debug.Log ("Beginning turn " + turnCount);
-			Debug.Log ("press space...");
+			if (turnCount % turnOrder.Count == 0) {
+				// all battlers have taken their turn; increment rounds
+				turnCount = 0;
+				roundCount += 1;
+				Debug.Log ("====== ROUND " + roundCount + " ======");
+			}
+			// increment turns
+			turnCount += 1;
+
+			// get current battler in trun order and take them out of the order
+			currentBattler = turnOrder [0];
+			turnOrder.RemoveAt (0);
+
+			// take their turn
+			Debug.Log("---- TURN" + turnCount + ": " + currentBattler.nickname + " ----");
+			//Debug.Log ("[space]");
+			//yield return null;
+			//yield return new WaitUntil (() => Input.GetButtonDown ("Jump"));
+			ProcessTurn (currentBattler);
+			Debug.Log ("[space]");
 			yield return null;
 			yield return new WaitUntil (() => Input.GetButtonDown ("Jump"));
 
-			turnCount += 1;
+			// add them back at the end of the order
+			turnOrder.Add (currentBattler);
 
 			if (CheckForWinner (out winner) == true) {
 				complete = true;
@@ -45,7 +74,8 @@ public class BattleController : MonoBehaviour {
 	}
 
 	void HandleAftermath () {
-		Debug.Log ("Battle completed; " + winner.nickname + " is the winner!");
+		Debug.Log ("======== BATTLE COMPLETED ========");
+		Debug.Log (winner.nickname + " is the winner!");
 		Debug.Log ("Returning to Field mode.");
 		game.state = GameController.GameState.Field;
 	}
@@ -68,5 +98,37 @@ public class BattleController : MonoBehaviour {
 		//  there is exactly one undefeated team; the battle is over
 		//  or there is no undefeated team; battle ends as draw
 		return true;
+	}
+
+	void CreateTurnOrder () {
+		turnOrder = new List<Battler> ();
+		List<Battler> turns = new List<Battler> ();
+		for (int i = 0; i < teams.Length; i++) {
+			for(int j = 0; j < teams[i].battlers.Length; j++) {
+				turns.Add(teams[i].battlers[j]);
+			}
+		}
+
+		int index;
+		while (turns.Count > 0) {
+			index = Random.Range (0, turns.Count);
+			turnOrder.Add (turns [index]);
+			turns.RemoveAt (index);
+		}
+	}
+
+	void ProcessTurn (Battler battler) {
+		List<Battler> targets = new List<Battler> ();
+		for (int i = 0; i < teams.Length; i++) {
+			if (teams [i] != battler.team) {
+				targets.AddRange (teams [i].battlers);
+			}
+		}
+
+		// pick random target
+		Battler target = targets[Random.Range(0, targets.Count)];
+
+		// attack target
+		battler.Attack(target);
 	}
 }
--- Assets/_Scripts/BeginBattleInteractable.cs
+++ Assets/_Scripts/BeginBattleInteractable.cs
@@ -6,7 +6,6 @@ public class BeginBattleInteractable : Interactable {
 	public BattleController battle;
 
 	public override void Interact () {
-		Debug.Log ("Beginning Battle...");
 		battle.Begin ();
 	}
 
