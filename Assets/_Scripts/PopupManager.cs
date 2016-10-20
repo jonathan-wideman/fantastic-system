e35ef39b3b1cda0397207f5e6633cd298f39235b
jonathan-wideman <jonathan.m.wideman@gmail.com>
2016-10-20 16:08:45 -0400

    Added Popup Manager to control creation of popups

---
+++ Assets/_Scripts/PopupManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopupManager : MonoBehaviour {
+
+	public TextPopup damageTextPrefab;
+
+	public TextPopup DamagePopup(float damage, Vector3 position, Transform parentTo = null) {
+		TextPopup popup = Instantiate (damageTextPrefab);
+		if (parentTo != null) {
+			popup.transform.SetParent (parentTo);
+			popup.transform.localPosition = position;
+		} else {
+			popup.transform.position = position;
+		}
+		popup.textComponent.text = "" + damage;
+		return popup;
+	}
+
+}
